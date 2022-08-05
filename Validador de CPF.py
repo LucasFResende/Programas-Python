adc07c05a72b2fda331d9a08b0cf4a073ed59445
LucasFResende <110567232+LucasFResende@users.noreply.github.com>
2022-08-05 13:00:12 -0300

    Add files via upload

--- Validador de CPF.py
+++ Validador de CPF.py
@@ -1,12 +1,16 @@
+alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
 cpf = input('Digite um CPF: ')
+cpf = cpf.lower()
 cpf = cpf.replace('.', '')
 cpf = cpf.replace('-', '')
+
+for c in alfabeto:
+    while c in cpf or len(cpf) != 11:
+        cpf = input('Digite um CPF válido: ')
+
 novo_cpf = cpf[:-2]
-
-if len(cpf) < 11:
-    print('CPF está incompleto')
-    exit()
-
 n = 10
 
 while True:
@@ -30,10 +34,11 @@ while True:
 
     if len(novo_cpf) == 11:
         if novo_cpf == cpf:
-            print('O CPF {0}.{1}.{2}-{3} é válido.'.format(novo_cpf[0:3],novo_cpf[3:6],novo_cpf[6:9],novo_cpf[9:]))
+            print('O CPF {0}.{1}.{2}-{3} é válido.'.format(novo_cpf[0:3], novo_cpf[3:6], novo_cpf[6:9], novo_cpf[9:]))
             break
         else:
-            print('O CPF {0}.{1}.{2}-{3} não é válido'.format(novo_cpf[0:3],novo_cpf[3:6],novo_cpf[6:9],novo_cpf[9:]))
+            print(
+                'O CPF {0}.{1}.{2}-{3} não é válido'.format(novo_cpf[0:3], novo_cpf[3:6], novo_cpf[6:9], novo_cpf[9:]))
             break
     else:
         continue
